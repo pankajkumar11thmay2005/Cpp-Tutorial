04bafa02298a1d0dbf4b55c4e1ad7c1b93862422
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-04-04 11:11:24 +0530

    Add files via upload

---
+++ Arrays/operations on array/traverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {2, 4, 8, 12, 16};
+    
+    // Traversing and printing arr
+    for (int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+    
+    return 0;
+}
